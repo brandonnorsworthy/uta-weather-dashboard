70a5df5ed5f9f8fbadcdbe8768bbcdc78a62bbfc
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-07-04 00:19:31 -0500

    lots of styling progress current city tagged up and formatted

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 
 // Global Variables
 var openWeatherAppId = `2c4a921d55c896205bdca23294d0393d`
-var currentCity
 
 // search => presented with current and future conditions
 // uv index => color indicating favorable, moderate, severe
@@ -18,9 +17,9 @@ function callCurrentWeatherDataAPI(cityName) {
     fetch(url)
     .then(response => response.json())
     .then(data => {
-        console.log(data.coord.lon, data.coord.lat);
+        // console.log(data.coord.lon, data.coord.lat);
         cityName = data.name;
-        console.log(`callCurrentWeatherDataAPI: `, cityName);
+        // console.log(`callCurrentWeatherDataAPI: `, cityName);
         callOneCallAPI(cityName, data.coord.lon, data.coord.lat);
     });
 
@@ -28,26 +27,26 @@ function callCurrentWeatherDataAPI(cityName) {
 }
 
 function callOneCallAPI(cityName, longitude, latitude) {
-    var url = `https://api.openweathermap.org/data/2.5/onecall?lon=${longitude}&lat=${latitude}&appid=${openWeatherAppId}`
+    var url = `https://api.openweathermap.org/data/2.5/onecall?units=imperial&lon=${longitude}&lat=${latitude}&appid=${openWeatherAppId}`
     fetch(url)
     .then(response => response.json())
     .then(data => {
-        console.log(`callOneCallAPI: `, data);
+        // console.log(`callOneCallAPI: `, data);
         displayCurrentWeather(cityName, data.current);
     });
 }
 
 function displayCurrentWeather(cityName, currentWeather) {
-    console.log(`displayCurrentWeather: `, currentWeather);
+    // console.log(`displayCurrentWeather: `, currentWeather);
     // current conditions => city name, date, icon of weather conditions, temp, humidity, wind speed, uv index
     //city name
     console.log(`City Name: `, cityName);
     //date
     console.log(`Date: `, moment().format(`M/D/YYYY`));
     //icon of weather conditions
-    var imgEl = document.createElement(`img`);
-    imgEl.setAttribute(`src`, `http://openweathermap.org/img/wn/${currentWeather.weather[0].icon}@2x.png`);
-    document.querySelector(`body`).appendChild(imgEl)
+    // var imgEl = document.createElement(`img`);
+    // imgEl.setAttribute(`src`, `http://openweathermap.org/img/wn/${currentWeather.weather[0].icon}@2x.png`);
+    // document.querySelector(`body`).appendChild(imgEl)
     //temp
     console.log(`Temp: `, currentWeather.temp);
     //humidity
@@ -58,4 +57,4 @@ function displayCurrentWeather(cityName, currentWeather) {
     console.log(`UV: `, currentWeather.uvi);
 }
 
-callCurrentWeatherDataAPI(`houston`)
+callCurrentWeatherDataAPI(`new york`)
